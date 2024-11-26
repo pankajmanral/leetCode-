efadd48e217d30253dc397df42f8d33869343ea5
pankajmanral <pankajmanral0208@gmail.com>
2024-11-26 20:30:13 +0530

    'removeDuplicates'

---
+++ Array String/removeDuplicates-1.py
@@ -0,0 +1,28 @@
+'''
+
+here,
+    the logic here is to compare the two elements 
+    if the two elements are same then just increment the i but if not then push the non matching element inside the k array and print k
+    nums[:k]
+
+    insert the last element inside the array -> nums[k] = nums[-1]
+                                                k += 1
+
+'''
+
+def removeDuplicates(nums:list) -> int:
+    k = 0
+    for i in range(len(nums)-1):
+        if nums[i] != nums[i+1]:
+            nums[k] = nums[i]
+            k += 1
+    nums[k] = nums[-1]
+    k+=1
+    print(nums[:k])
+    return k
+    
+nums = [0,0,1,1,1,2,2,3,3,4,4]
+
+result = removeDuplicates(nums)
+
+print(result)
